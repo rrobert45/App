fa30fe5c7337575d8f8134e2bc9a6808fbc83ea8
rrobert45 <rrobert45@gmail.com>
2023-01-24 16:40:10 -0500

    update

--- static/script.js
+++ static/script.js
@@ -1,12 +1,15 @@
-function updateValue(variable, value) {
-    $.ajax({
-        type: 'POST',
-        url: '/update_settings',
-        data: JSON.stringify({ variable: variable, value: value }),
-        contentType: 'application/json; charset=utf-8',
-        dataType: 'json',
-        success: function (response) {
-            alert('Successfully updated ' + variable + ' to ' + value);
-        }
-    });
+function updateValue(variable) {
+    let newValue = prompt("Enter the new value for " + variable);
+    if (newValue != null) {
+        $.ajax({
+            type: 'POST',
+            url: '/update_settings',
+            data: JSON.stringify({ variable: variable, value: newValue }),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success: function (response) {
+                alert('Successfully updated ' + variable + ' to ' + newValue);
+            }
+        });
+    }
 }
