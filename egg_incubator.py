dbe5ef1b0b8a3c5c60f20002560f33f0944058e7
rrobert45 <rrobert45@gmail.com>
2023-02-27 15:15:14 -0500

    Update egg_incubator.py

--- egg_incubator.py
+++ egg_incubator.py
@@ -54,55 +54,50 @@ GPIO.setup(heat_relay_pin, GPIO.OUT)
 GPIO.setup(humidifier_relay_pin, GPIO.OUT)
 GPIO.setup(egg_turner_relay_pin, GPIO.OUT)
 
-def read_sensor_data():
-    # Read the humidity and temperature
-    humidity, temperature = sensor.relative_humidity, sensor.temperature
-    if humidity is not None and temperature is not None:
-        temperature = (temperature * 9/5) + 32
-        return round(temperature,1), round(humidity,1)
-    else:
-        print('Failed to read data from sensor')
-        return None, None
 
 
-def log_data(temperature, humidity, last_relay_on, temperature_relay_status, humidity_relay_status, day_in_cycle):
-    # Get the most recent record from the database
-    last_record = incubator.find_one(sort=[('_id', pymongo.DESCENDING)])
 
-    # Check if a record has been stored within the log interval
-    if last_record is None or (datetime.now() - datetime.strptime(last_record['Time'], '%m-%d-%Y %H:%M')).total_seconds() >= log_interval:
-        # Create a data dictionary
-        data = {
-            'Time': time.strftime("%m-%d-%Y %H:%M"),
-            'Temperature(F)': temperature,
-            'Temperature Relay Status': temperature_relay_status,
-            'Humidity(%)': humidity,
-            'Humidity Relay Status': humidity_relay_status,
-            'Last Egg Turn': last_relay_on.strftime("%m-%d-%Y %I:%M %P") if last_relay_on is not None else '',
-            'Day in Egg Cycle': day_in_cycle
-        }
+def read_and_log_data():
+    global temperature_relay_status
+    global humidity_relay_status
+    day_in_cycle = day(start_date)
 
-        # Insert the data into the incubator collection
-        incubator.insert_one(data)
+    while True:
+        try:
+            temperature, humidity = control(day_in_cycle)
+            last_relay_on = eggTurner(day_in_cycle)
+            log_data(temperature, humidity, last_relay_on, temperature_relay_status, humidity_relay_status, day_in_cycle)
+            time.sleep(20)
+            
+        except KeyboardInterrupt:
+            break
+        except:
+            print("An error occurred. Retrying in 30 seconds...")
+            time.sleep(30)
+            continue
+    # Clean up the GPIO pins
+    GPIO.cleanup()
+    # Close the MongoDB connection
+    client.close()
+
+def day(start_date):
+    global humidity_threshold
+    current_date = datetime.now()
+    total_days = 21
+    day_in_cycle = (current_date - start_date).days % total_days
+    if day_in_cycle >= 18:
+        humidity_threshold = 75
+    return day_in_cycle
 
 
-def eggTurner(day_in_cycle):
-    global last_relay_on
-    
-    current_time = datetime.now()
-    if day_in_cycle < 18:
-        if last_relay_on is None:
-            last_relay_on = datetime.now()
-        if GPIO.input(egg_turner_relay_pin) == 1:
-            if current_time - last_relay_on >= timedelta(seconds=relay_interval):
-                # Turn on the relay for 2 minutes
-                GPIO.output(egg_turner_relay_pin, GPIO.LOW)
-                last_relay_on = current_time
-        elif GPIO.input(egg_turner_relay_pin) == 0:        
-            if current_time - last_relay_on >= timedelta(seconds=roll_interval):
-                GPIO.output(egg_turner_relay_pin, GPIO.HIGH)
-    return last_relay_on
-
+def day_for_web(start_date):
+    global humidity_threshold
+    current_date = datetime.now()
+    total_days = 21
+    day_in_cycle = (current_date - start_date).days % total_days
+    if day_in_cycle >= 18:
+        humidity_threshold = 75
+    return day_in_cycle
 
 def control():
     global temperature_relay_status
@@ -135,58 +130,52 @@ def control():
             humidity_relay_status = "OFF"
     return temperature, humidity
 
+def read_sensor_data():
+    # Read the humidity and temperature
+    humidity, temperature = sensor.relative_humidity, sensor.temperature
+    if humidity is not None and temperature is not None:
+        temperature = (temperature * 9/5) + 32
+        return round(temperature,1), round(humidity,1)
+    else:
+        print('Failed to read data from sensor')
+        return None, None
+
+def eggTurner(day_in_cycle):
+    global last_relay_on
     
+    current_time = datetime.now()
+    if day_in_cycle < 18:
+        if last_relay_on is None:
+            last_relay_on = datetime.now()
+        if GPIO.input(egg_turner_relay_pin) == 1:
+            if current_time - last_relay_on >= timedelta(seconds=relay_interval):
+                # Turn on the relay for 2 minutes
+                GPIO.output(egg_turner_relay_pin, GPIO.LOW)
+                last_relay_on = current_time
+        elif GPIO.input(egg_turner_relay_pin) == 0:        
+            if current_time - last_relay_on >= timedelta(seconds=roll_interval):
+                GPIO.output(egg_turner_relay_pin, GPIO.HIGH)
+    return last_relay_on
 
+def log_data(temperature, humidity, last_relay_on, temperature_relay_status, humidity_relay_status, day_in_cycle):
+    # Get the most recent record from the database
+    last_record = incubator.find_one(sort=[('_id', pymongo.DESCENDING)])
 
-def day():
-    global humidity_threshold
-    current_date = datetime.now()
-    total_days = 21
-    day_in_cycle = (current_date - start_date).days % total_days
-    if day_in_cycle >= 18:
-        humidity_threshold = 75
-    return day_in_cycle
+    # Check if a record has been stored within the log interval
+    if last_record is None or (datetime.now() - datetime.strptime(last_record['Time'], '%m-%d-%Y %H:%M')).total_seconds() >= log_interval:
+        # Create a data dictionary
+        data = {
+            'Time': time.strftime("%m-%d-%Y %H:%M"),
+            'Temperature(F)': temperature,
+            'Temperature Relay Status': temperature_relay_status,
+            'Humidity(%)': humidity,
+            'Humidity Relay Status': humidity_relay_status,
+            'Last Egg Turn': last_relay_on.strftime("%m-%d-%Y %I:%M %P") if last_relay_on is not None else '',
+            'Day in Egg Cycle': day_in_cycle
+        }
 
-
-def lock_down_and_hatch():
-    lock_down_date = start_date + timedelta(days=18)
-    hatch_date = start_date + timedelta(days=21)
-    return lock_down_date,hatch_date
-
-
-def update_config(variable, value):
-    with open("config.json", "r") as config_file:
-        config = json.load(config_file)
-        config[variable] = value
-    with open("config.json", "w") as config_file:
-        json.dump(config, config_file)
-
-def clear_database():
-    incubator.drop()
-
-
-def read_and_log_data():
-    global temperature_relay_status
-    global humidity_relay_status
-
-    while True:
-        try:
-            day_in_cycle = day()
-            temperature, humidity= control()
-            last_relay_on = eggTurner(day_in_cycle)
-            log_data(temperature, humidity, last_relay_on, temperature_relay_status, humidity_relay_status, day_in_cycle)
-            time.sleep(20)
-            
-        except KeyboardInterrupt:
-            break
-        except:
-            print("An error occurred. Retrying in 30 seconds...")
-            time.sleep(30)
-            continue
-    # Clean up the GPIO pins
-    GPIO.cleanup()
-    # Close the MongoDB connection
-    client.close()
+        # Insert the data into the incubator collection
+        incubator.insert_one(data)
 
 def get_egg_cycle_statistics(historical_data):
     egg_cycle_dict = {}
@@ -220,14 +209,30 @@ def get_egg_cycle_statistics(historical_data):
     egg_cycle_statistics.sort(key=lambda x: x['Day in Egg Cycle'], reverse=True)
     return egg_cycle_statistics
 
+def lock_down_and_hatch(start_date):
+    lock_down_date = start_date + timedelta(days=18)
+    hatch_date = start_date + timedelta(days=21)
+    return lock_down_date,hatch_date
+
+
+def update_config(variable, value):
+    with open("config.json", "r") as config_file:
+        config = json.load(config_file)
+        config[variable] = value
+    with open("config.json", "w") as config_file:
+        json.dump(config, config_file)
+
+def clear_database():
+    incubator.drop()
 
 @app.route("/")
 def index():
-    day_in_cycle = day()
-    last_relay_on = eggTurner(day_in_cycle)
+    day_in_cycle = day_for_web(start_date)
     lock_down_date,hatch_date = lock_down_and_hatch()
     temperature, humidity = read_sensor_data()
     last_relay_on = last_relay_on.strftime("%m-%d-%Y %I:%M %P") if last_relay_on is not None else ''
+    last_record = incubator.find_one(sort=[('_id', pymongo.DESCENDING)], projection={'Last Egg Turn': 1})
+    last_relay_on = last_record.get('Last Egg Turn', '')
     cursor = incubator.find().limit(48).sort("Time", -1)
     historical_data = []
     
